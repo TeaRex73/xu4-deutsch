65de245a838a1602f3a4b6adbf346d54a5227c32
Linards Ticmanis <ticmanis@gmx.de>
2025-01-12 21:39:46 +0100

    Download ultima4.zip from a random German Gentoo mirror, since dowloading from thatflemminggent.com doesn't work on Windows 7 (HTTPS incompatibility)

--- u4.bat
+++ u4.bat
@@ -1,28 +1,38 @@
 @echo off
+verify other 2>nul
+setlocal enableextensions
+if errorlevel 1 goto noextensions
+setlocal disabledelayedexpansion
 cd /d "%~dp0"
-if not exist "%APPDATA%\xu4\nul" mkdir "%APPDATA%\xu4"
-copy /y .\xu4.cfg "%PPDATA%\xu4"
+if not exist "%APPDATA%\xu4" mkdir "%APPDATA%\xu4" >nul 2>nul
+copy /y .\xu4.cfg "%APPDATA%\xu4" >nul 2>nul
 if not exist .\ultima4.zip goto download
-attrib -s -h -r "%TEMP%\u4zipsiz.txt" 2>nul
-del /f "%TEMP%\u4zipsiz.txt" 2>nul
+attrib -s -h -r "%TEMP%\u4zipsiz.txt" >nul 2>nul
+del /f "%TEMP%\u4zipsiz.txt" >nul 2>nul
 call :size .\ultima4.zip >"%TEMP%\u4zipsiz.txt"
 set /p FILESIZE=<"%TEMP%\u4zipsiz.txt"
-del /f "%TEMP%\u4zipsiz.txt"
+del /f "%TEMP%\u4zipsiz.txt" >nul 2>nul
 if not "%FILESIZE%"=="529099" goto download
 goto nodownload
 :download
-msg Console /w /time:9999 "Die originale englischsprachige MS-DOS-Version von Ultima IV wird jetzt heruntergeladen..."
-attrib -s -h -r .\ultima4.zip 2>nul
-del /f .\ultima4.zip 2>nul
-powershell -c "& {(New-Object System.Net.WebClient).DownloadFile('https://ultima.thatfleminggent.com/ultima4.zip','ultima4.zip')}"
-attrib -s -h -r "%TEMP%\u4zipsiz.txt" 2>nul
-del /f "%TEMP%\u4zipsiz.txt" 2>nul
-call :size .\ultima4.zip >"%TEMP%\u4zipsiz.txt"
-set /p FILESIZE=<"%TEMP%\u4zipsiz.txt"
-del /f "%TEMP%\u4zipsiz.txt"
-if not "%FILESIZE%"=="529099" goto error
-msg Console /w /time:9999 "Herunterladen war erfolgreich! Klicke erneut auf Ultima IV, um die Dokumentation lesbar zu machen!"
-goto :eof
+cscript /nologo MessageBox.vbs "Die originale MS-DOS-Version von Ultima IV wird jetzt heruntergeladen..."
+set TRY=1
+:nexttry
+call mirrors.bat
+set /a m=%RANDOM% %% %MAXMIRROR% + 1
+set MIRROR=MIRROR%m%
+setlocal enabledelayedexpansion
+call :getfile !%MIRROR%!
+if errorlevel 1 goto error
+setlocal disabledelayedexpansion
+cscript /nologo MessageBox.vbs "Herunterladen war erfolgreich! Klicke erneut auf Ultima IV, um die Dokumentation lesbar zu machen!"
+exit /b 0
+:error
+setlocal disabledelayedexpansion
+set /a TRY=%TRY% + 1
+if %TRY% LEQ 10 goto :nexttry
+cscript /nologo MessageBox.vbs "Herunterladen war nicht erfolgreich! Lade Ultima IV von https://ultima.thatfleminggent.com/ultima4.zip selbst herunter und kopiere die zip-Datei, ohne sie zu entpacken, in den Unterordner Daten!"
+exit /b 1
 :nodownload
 cd "..\Ultima IV Dokumentation"
 if not exist Box.pdf goto decode
@@ -32,34 +42,43 @@ if not exist Referenz.pdf goto decode
 if not exist Weisheit.pdf goto decode
 goto nodecode
 :decode
-msg Console /w /time:9999 "Die Dokumentation wird nun lesbar gemacht..."
+cscript /nologo MessageBox.vbs "Die Dokumentation wird nun lesbar gemacht..."
 for %%I in (*.xor) do ..\Daten\xorbin.exe %%I ..\Daten\ultima4.zip > %%~dpnI
 if not exist Box.pdf goto error2
-if not exist Geschichte.pdf goto error2
+if not exist Geschichte.pdf goto error
 if not exist Karte.pdf goto error2
 if not exist Referenz.pdf goto error2
 if not exist Weisheit.pdf goto error2
-del /f /q *.xor 2>nul
+del /f /q *.xor >nul 2>nul
 cd /d "%~dp0"
-msg Console /w /time:9999 "Lesbarmachen war erfolgreich, bitte lies die PDF-Dateien im Ordner Ultima IV Dokumentation! Klicke nochmal auf Ultima IV, um zu spielen!"
-goto :eof
+cscript /nologo MessageBox.vbs "Lesbarmachen war erfolgreich, bitte lies die PDF-Dateien im Ordner Ultima IV Dokumentation! Klicke nochmal auf Ultima IV, um zu spielen!"
+exit /b 0
 :nodecode
 cd /d "%~dp0"
 set SDL_VIDEODRIVER=windib
 start /b /min .\u4.exe -f
-goto :eof
-:error
-cd /d "%~dp0"
-msg Console /w /time:9999 "Herunterladen war nicht erfolgreich! Finde im Internet die Datei ultima4.zip, die 529.099 Bytes gross ist, und kopiere sie in den versteckten Unterordner Daten!"
-goto :eof
+exit /b 0
 :error2
 cd /d "%~dp0"
-msg Console /w /time:9999 "Die Dokumentation konnte nicht lesbar gemacht werden! Bitte kontaktiere die Entwickler und melde den Fehler!"
-goto :eof
+cscript /nologo MessageBox.vbs "Die Dokumentation konnte nicht lesbar gemacht werden! Bitte kontaktiere die Entwickler und melde den Fehler!"
+exit /b 1
+:getfile
+attrib -s -h -r .\ultima4.zip >nul 2>nul
+del /f .\ultima4.zip >nul 2>nul
+powershell -c "& {(New-Object System.Net.WebClient).DownloadFile('%1distfiles/59/ultima4.zip','ultima4.zip')}"
+attrib -s -h -r "%TEMP%\u4zipsiz.txt" >nul 2>nul
+del /f "%TEMP%\u4zipsiz.txt" >nul 2>nul
+call :size .\ultima4.zip >"%TEMP%\u4zipsiz.txt"
+set /p FILESIZE=<"%TEMP%\u4zipsiz.txt"
+del /f "%TEMP%\u4zipsiz.txt" >nul 2>nul
+if "%FILESIZE%"=="529099" exit /b 0
+exit /b 1
 :size
-if exist "%1" goto cont
+if exist %1 goto cont
 echo 0
-goto :eof
+exit /b
 :cont
 echo %~z1
-goto :eof
+exit /b
+:noextensions
+echo Windows-Version ist nicht kompatibel!
